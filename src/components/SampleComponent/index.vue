feca253bb7149a1626d1bc0c9db2e7f284a7eea9
Ali <aliafsah1988@gmail.com>
2020-01-05 23:45:21 +0330

    SampleComponent added

---
+++ src/components/SampleComponent/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sample">
+    <h1>Some text that is floated !</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SampleComponent",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style>
+@import url("./style.css");
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home" :class="{ rtl: isRightToLeft, ltr: !isRightToLeft }">
     <img alt="Vue logo" src="../../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SampleComponent msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import SampleComponent from "@/components/SampleComponent";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    SampleComponent
   },
   data() {
     return {
